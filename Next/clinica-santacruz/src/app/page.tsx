cda94b6a01fb341a4913990b4e6cc2742c42cf20
Lucas Brito <britolmbs@hotmail.com>
2025-09-08 17:27:30 -0300

    Feat: Criando Pagina principal

--- Next/clinica-santacruz/src/app/page.tsx
+++ Next/clinica-santacruz/src/app/page.tsx
@@ -1,4 +1,6 @@
-import { SquareActivity } from "lucide-react";
+import { FeatureCard } from "@/components/FeatureCard";
+import { Button } from "@/components/ui/button";
+import { CalendarClock, Heart, Hospital, SquareActivity, Zap } from "lucide-react";
 
 
 export default function Home() {
@@ -15,7 +17,43 @@ export default function Home() {
       <main className="flex-1 flex items-center justify-center">
         <div className="container mx-auto px-4 py-12 md:py-24">
           <div className="max-w-3xl mx-auto">
-            
+            <div className="text-center space-y-6">
+              <div className="inline-block bg-black text-white px-4 py-1.5 rounded-full text-sm font-medium md-2">
+                Sistema de Agendamento de Consulta
+              </div>
+              <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-950 to-indigo-800">Rapido e Facil de forma Descoplicada</h1>
+              <p className="text-lg text-gray-600 max-w-2xl mx-auto">Verifique o Agendamento e Dashboard em uma pagina personalizada e elegante, sem complicações</p>            
+            <div className="pt-4">
+              <form action=''>
+            <Button type="submit"
+            size='lg'
+            className="bg-indigo-950 hover:bg-indigo-800 text-white font-medium px-8 h-12"
+            >
+              Acessar Agora
+              <CalendarClock className="ml-12 h-4 w-4"/>
+              </Button>
+              </form>
+            </div>
+            </div>
+
+            <div className="mt-16 grid grid-cols-1 md:grid-cols-3 gap-6">
+              <FeatureCard
+              icon={<Zap className="h-8 w-8 text-indigo-800" />}
+              title=""
+              description=""
+              />
+              <FeatureCard
+              icon={<Heart className="h-8 w-8 text-indigo-800" />}
+              title=""
+              description=""
+              />
+              <FeatureCard
+              icon={<Hospital className="h-8 w-8 text-indigo-800" />}
+              title=""
+              description=""
+              />
+
+            </div>
           </div>
         </div>
       </main>
