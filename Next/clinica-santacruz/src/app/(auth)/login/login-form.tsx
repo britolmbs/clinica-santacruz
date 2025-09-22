1f617fad764105535f209e540b7bdcb1a1e8db54
Lucas Brito <britolmbs@hotmail.com>
2025-09-22 16:45:54 -0300

    Feat: Tratando Erros de login

--- Next/clinica-santacruz/src/app/(auth)/login/login-form.tsx
+++ Next/clinica-santacruz/src/app/(auth)/login/login-form.tsx
@@ -4,11 +4,19 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Form from "next/form";
 import loginAction from "./loginAction";
+import { useActionState } from "react";
 
 export default function LoginForm() {
+    const [ state, formAction, isPending] = useActionState(loginAction, null);
     return (
-        <>
-         <Form action={loginAction}>
+       <>
+       {state?.success === false && (
+        <div role="alert" className="text-xs mb-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+            <strong className="font-bold">Erro! </strong>
+            <span className="block sm:inline">{state?.message}</span>
+        </div>
+       )}
+         <Form action={formAction}>
               <div>
                 <Label>Usuario</Label>
                 <Input type="text" name="usuario" placeholder="Digite seu Usuario" />
--- Next/clinica-santacruz/src/app/(auth)/login/loginAction.ts
+++ Next/clinica-santacruz/src/app/(auth)/login/loginAction.ts
@@ -2,7 +2,20 @@
 
 import { signIn } from "@/lib/auth"
 
-export default async function loginAction(formData: FormData) {
+export default async function loginAction(_prevState: any, formData: FormData) {
+try {
+     await signIn("credentials", {
+        username: formData.get('user') as string,
+        password: formData.get('password') as string,
+        redirect: false,
+        });
 
-    await signIn("credentials", formData);
+     return { success: true };
+} catch (err) {
+    if(err.type === 'CredentialsSignin') {
+     return { success : false, message: 'Usuario ou Senha Invalida'};
+    }
+    return { success: false, message: 'Ops, Algo de Errado' };
+}
+   
 }
